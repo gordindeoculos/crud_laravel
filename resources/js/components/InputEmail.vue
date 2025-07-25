a65fca3335d2da0de7c82e57efba9340c267d2bb
Renato Gomes <renatotg10@gmail.com>
2025-07-25 15:59:32 -0300

    Ajustado o compomente vue InputEmail para verificar quando o input perde o foco

--- resources/js/components/InputEmail.vue
+++ resources/js/components/InputEmail.vue
@@ -1,10 +1,9 @@
 <template>
     <div class="mb-3 col-6">
         <label class="form-label">E-mail</label>
-        <input class="form-control" type="text" v-model="email" placeholder="Informe seu e-mail">
+        <input class="form-control" type="text" v-model="email" placeholder="Informe seu e-mail" @blur="validar">
         <p v-if="erro" class="text-danger small mt-1">{{ erro }}</p>
         <button class="btn btn-primary mt-2" @click="validar">Enviar</button>
-        
     </div>
 </template>
 
@@ -28,7 +27,6 @@ export default {
                 this.erro = 'Este campo é obrigatório.';
             } else {
                 this.erro = '';
-                alert('Enviado com sucesso!');
             }
         }
     }
